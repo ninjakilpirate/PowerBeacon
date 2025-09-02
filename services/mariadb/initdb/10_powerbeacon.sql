ab8bc7de7e4a28d095c521c2f5393c7799461866
ninjakilpirate <nathan.grosse@Gmail.com>
2025-09-02 16:58:12 -0700

    Manages Docker More Better

---
+++ services/mariadb/initdb/10_powerbeacon.sql
@@ -0,0 +1,195 @@
+-- MariaDB dump 10.19  Distrib 10.11.4-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: powerbeacon
+-- ------------------------------------------------------
+-- Server version	10.11.4-MariaDB-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE IF NOT EXISTS `powerbeacon` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+--
+-- Table structure for table `callbackAddresses`
+--
+
+DROP TABLE IF EXISTS `callbackAddresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `callbackAddresses` (
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `verify_key` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `callbackAddresses`
+--
+
+LOCK TABLES `callbackAddresses` WRITE;
+/*!40000 ALTER TABLE `callbackAddresses` DISABLE KEYS */;
+INSERT INTO `callbackAddresses` VALUES
+('Default','http://127.0.0.1','MTcxMzkxMjk1NS42MTg5Nzc4');
+/*!40000 ALTER TABLE `callbackAddresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkins`
+--
+
+DROP TABLE IF EXISTS `checkins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checkins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `UUID` varchar(50) DEFAULT NULL,
+  `gateway` varchar(30) DEFAULT NULL,
+  `last_checkin` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `UUID` (`UUID`),
+  CONSTRAINT `checkins_ibfk_1` FOREIGN KEY (`UUID`) REFERENCES `implants` (`UUID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkins`
+--
+
+LOCK TABLES `checkins` WRITE;
+/*!40000 ALTER TABLE `checkins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `checkins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `datastore`
+--
+
+DROP TABLE IF EXISTS `datastore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `datastore` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `UUID` varchar(50) DEFAULT NULL,
+  `delivered` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `data` longtext DEFAULT NULL,
+  `details` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UUID` (`UUID`),
+  CONSTRAINT `datastore_ibfk_1` FOREIGN KEY (`UUID`) REFERENCES `implants` (`UUID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `datastore`
+--
+
+LOCK TABLES `datastore` WRITE;
+/*!40000 ALTER TABLE `datastore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datastore` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `implants`
+--
+
+DROP TABLE IF EXISTS `implants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `implants` (
+  `UUID` varchar(50) NOT NULL,
+  `implantkey` varchar(50) DEFAULT NULL,
+  `notes` varchar(250) DEFAULT NULL,
+  `C2` varchar(250) DEFAULT NULL,
+  `filter` varchar(250) DEFAULT NULL,
+  `consumer` varchar(250) DEFAULT NULL,
+  `implant_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`UUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `implants`
+--
+
+LOCK TABLES `implants` WRITE;
+/*!40000 ALTER TABLE `implants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `implants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_type` varchar(50) DEFAULT NULL,
+  `log` text DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  `UUID` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logs`
+--
+
+LOCK TABLES `logs` WRITE;
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task` varchar(8000) DEFAULT NULL,
+  `time_complete` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `UUID` varchar(100) DEFAULT NULL,
+  `is_complete` tinyint(1) DEFAULT 0,
+  `notes` varchar(2500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UUID` (`UUID`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`UUID`) REFERENCES `implants` (`UUID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-31 17:16:03
